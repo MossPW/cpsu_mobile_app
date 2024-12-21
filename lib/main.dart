9134bbd846b6a67452f7a2536c916ecb2390d9b4
Patipan Watjanapron <patipan.wat>
2024-12-21 13:28:13 +0700

    Change text.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo2',
+        title: 'Flutter Demo_X',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(useMaterial3: true, primarySwatch: Colors.blue),
         home: GreetingWidget());
